95f0597fa83531ecd2499b7e66774ea2c1425590
abdallahelmadi <abdallahelmadi42@gmail.com>
2025-09-09 12:09:03 +0100

    1 line of norminette

--- tools/rerenderinit_utils.c
+++ tools/rerenderinit_utils.c
@@ -6,7 +6,7 @@
 /*   By: abdael-m <abdael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 17:52:18 by abdael-m          #+#    #+#             */
-/*   Updated: 2025/09/09 11:41:24 by abdael-m         ###   ########.fr       */
+/*   Updated: 2025/09/09 11:56:46 by abdael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,10 @@ void	rerenderinit_utils(t_globaldata *t, double *ray_angle, int line_index)
 	double (ray_dir_x), (ray_dir_y), (delta_dist_x), (delta_dist_y),
 	(side_dist_x), (side_dist_y), (perp_wall_dist), (corrected_dist),
 	(perp_wall_on_screen), (wall_hit_x_in_texture), (step_texture_step),
-	(tex_pos);
+	(tex_y);
 	int (map_x), (map_y), (step_x), (step_y), (side),
 	(texture_index), (start_draw), (end_draw),
-	(tex_x), (tex_y), (tindex);
+	(tex_x), (tindex);
 	if (*ray_angle > 2 * PI)
 		*ray_angle -= 2 * PI;
 	if (*ray_angle < 0)
@@ -79,7 +79,6 @@ void	rerenderinit_utils(t_globaldata *t, double *ray_angle, int line_index)
 		step_y = 1;
 		side_dist_y = (map_y + 1.0 - t->player.py / TILE_SIZE) * delta_dist_y;
 	}
-	side = 0;
 	while (1)
 	{
 		if (side_dist_x < side_dist_y)
@@ -124,19 +123,22 @@ void	rerenderinit_utils(t_globaldata *t, double *ray_angle, int line_index)
 	if (end_draw >= WIN_HEIGHT)
 		end_draw = WIN_HEIGHT - 1;
 	if (side == 0)
-		wall_hit_x_in_texture = t->player.py / TILE_SIZE + perp_wall_dist * ray_dir_y;
+		wall_hit_x_in_texture = t->player.py / TILE_SIZE \
+		+ perp_wall_dist * ray_dir_y;
 	else
-		wall_hit_x_in_texture = t->player.px / TILE_SIZE + perp_wall_dist * ray_dir_x;
+		wall_hit_x_in_texture = t->player.px / TILE_SIZE \
+		+ perp_wall_dist * ray_dir_x;
 	wall_hit_x_in_texture -= floor(wall_hit_x_in_texture);
 	tex_x = (int)(wall_hit_x_in_texture * t->wrapper[texture_index].width);
-	step_texture_step = (double)t->wrapper[texture_index].height / (double)perp_wall_on_screen;
-	tex_pos = (start_draw - WIN_HEIGHT / 2.0 + perp_wall_on_screen / 2.0) * step_texture_step;
+	step_texture_step = (double)t->wrapper[texture_index].height \
+		/ (double)perp_wall_on_screen;
+	tex_y = (start_draw - WIN_HEIGHT / 2.0 + perp_wall_on_screen \
+		/ 2.0) * step_texture_step;
 	tindex = start_draw - 1;
 	while (++tindex < end_draw)
 	{
-		tex_y = (int)tex_pos;
-		tex_pos += step_texture_step;
 		my_mlx_pixel_put(&t->img, line_index, tindex,
-				get_texture_color(&t->wrapper[texture_index], tex_x, tex_y));
+			get_texture_color(&t->wrapper[texture_index], tex_x, (int)tex_y));
+		tex_y += step_texture_step;
 	}
 }
